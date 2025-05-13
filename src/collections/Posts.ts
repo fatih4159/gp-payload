455644426485ef39c82f4bb740b4ca414e1673bd
Fatih Tuluk <admin@cynefa.com>
2025-05-13 17:53:24 +0000

    init

--- src/collections/Posts.ts
+++ src/collections/Posts.ts
@@ -2,7 +2,22 @@ import type { CollectionConfig } from 'payload'
 
 export const Posts: CollectionConfig = {
     slug: 'posts',
-    fields: [
+    admin: {
+      group: 'posts',
+      useAsTitle: 'title',
+      description: 'This is a blog collection' 
+    },
+    access: {
+      read: () => true,
+      create: () => true,
+      update: () => true,
+    },
+    defaultSort: ['title'],
+    labels: {
+      singular: 'Blog Post',
+      plural: 'Blog Posts',
+    },
+        fields: [
       {
         name: 'title',
         type: 'text',
